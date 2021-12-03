52e65e693bdb93045e2fbb920f157bf144cee780
bryanalv625 <81651971+bryanalv625@users.noreply.github.com>
2021-12-02 23:33:05 -0600

    Update DeleteController.java

--- src/application/controller/DeleteController.java
+++ src/application/controller/DeleteController.java
@@ -1,7 +1,7 @@
 /**
  * The DeleteController class is responsible for updating the delete view based on the user's inputs
  * 
- * 	@author Payton Chism (xja124), Beryl Mohanadhas (eaq312), Bryan Alvarado (zvy639)
+ * 	@author Payton Chism (xja124), Beryl Mohanadhas (eaq312), Valeria Villanueva (epi655), Bryan Alvarado (zvy639)
  */
 
 package application.controller;
