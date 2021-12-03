b0e51b140e911ac40a182f0287dd92bbd4387f11
bryanalv625 <81651971+bryanalv625@users.noreply.github.com>
2021-12-02 23:33:37 -0600

    Update UpdateController.java

--- src/application/controller/UpdateController.java
+++ src/application/controller/UpdateController.java
@@ -1,7 +1,7 @@
 /**
  * The UpdateController class is responsible for changing the Update view based on the user's inputs
  * 
- * 	@author Payton Chism (xja124), Beryl Mohanadhas (eaq312), Bryan Alvarado (zvy639)
+ * 	@author Payton Chism (xja124), Beryl Mohanadhas (eaq312), Valeria Villanueva (epi655), Bryan Alvarado (zvy639)
  */
 
 package application.controller;
