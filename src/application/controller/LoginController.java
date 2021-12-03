a859618f870050d4daa9c768971a2a02e75b8cee
bryanalv625 <81651971+bryanalv625@users.noreply.github.com>
2021-12-02 23:31:58 -0600

    Update LoginController.java

--- src/application/controller/LoginController.java
+++ src/application/controller/LoginController.java
@@ -1,7 +1,7 @@
 /**
  * The LoginController class is responsible for changing the Login view based on the user's inputs
  * 
- * 	@author Payton Chism (xja124), Beryl Mohanadhas (eaq312)
+ * 	@author Payton Chism (xja124), Beryl Mohanadhas (eaq312), Bryan Alvarado (zvy639)
  */
 package application.controller;
 
